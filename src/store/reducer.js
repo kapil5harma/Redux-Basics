7122d1c9ca5fb05345dc6b93addcd29732365bab
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-15 19:16:15 +0530

    Switch-Case in the Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,27 +2,27 @@ const initialState = {
   counter: 0
 };
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    };
-  }
-  if (action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    };
-  }
-  if (action.type === 'ADD') {
-    return {
-      counter: state.counter + action.payload.value
-    };
-  }
-  if (action.type === 'SUBTRACT') {
-    return {
-      counter: state.counter - action.payload.value
-    };
-  }
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1
+      };
 
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1
+      };
+
+    case 'ADD':
+      return {
+        counter: state.counter + action.payload.value
+      };
+
+    case 'SUBTRACT':
+      return {
+        counter: state.counter - action.payload.value
+      };
+  }
   return state;
 };
 
