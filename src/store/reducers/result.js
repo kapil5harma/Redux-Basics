5e557a5858c59a8fc7eadf6cd6012fc10c59e9a8
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-17 23:20:51 +0530

    A leaner Switch Case statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,46 +5,54 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  // One way to immutably remove an element from an array:
+  // const id = 2;
+  // const newArray = [...state.results];
+  // newArray.splice(id, 1);
+
+  // Another way (better way):
+  const updatedArray = state.results.filter(
+    result => result.id !== action.payload.resElID
+  );
+  // return {
+  //   ...state,
+  //   results: updatedArray
+  // };
+
+  // Making leaner reducers using utility function (updateObject).
+  return updateObject(state, {
+    results: updatedArray
+  });
+};
+
+const storeResult = (state, action) => {
+  // Push adds new item in original array.
+  // Concat creates a copy of actual array, adds the item, and returns the new array.
+  // return {
+  //   ...state,
+  //   results: state.results.concat({
+  //     id: new Date(),
+  //     value: action.payload.result
+  //   })
+  // };
+
+  // Making leaner reducers using utility function (updateObject).
+  return updateObject(state, {
+    results: state.results.concat({
+      id: new Date(),
+      value: action.payload.result
+    })
+  });
+};
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      // Push adds new item in original array.
-      // Concat creates a copy of actual array, adds the item, and returns the new array.
-      // return {
-      //   ...state,
-      //   results: state.results.concat({
-      //     id: new Date(),
-      //     value: action.payload.result
-      //   })
-      // };
-
-      // Making leaner reducers using utility function (updateObject).
-      return updateObject(state, {
-        results: state.results.concat({
-          id: new Date(),
-          value: action.payload.result
-        })
-      });
+      return storeResult(state, action);
 
     case actionTypes.DELETE_RESULT:
-      // One way to immutably remove an element from an array:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1);
-
-      // Another way (better way):
-      const updatedArray = state.results.filter(
-        result => result.id !== action.payload.resElID
-      );
-      // return {
-      //   ...state,
-      //   results: updatedArray
-      // };
-
-      // Making leaner reducers using utility function (updateObject).
-      return updateObject(state, {
-        results: updatedArray
-      });
+      return deleteResult(state, action);
 
     default:
       return state;
