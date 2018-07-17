e79bb998e82d77d9185b4d2b168803e28e34c207
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-17 23:10:19 +0530

    Usinf Utility functions

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   counter: 0
@@ -8,28 +9,45 @@ const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT:
       // Older way of immutably updating object (shallow copy an object and then add, update whatever you want to)
-      const newState = Object.assign({}, state);
-      newState.counter = state.counter + 1;
-      return newState;
+      // const newState = Object.assign({}, state);
+      // newState.counter = state.counter + 1;
+      // return newState;
+
+      // Making leaner reducers using utility function (updateObject).
+      return updateObject(state, {
+        counter: state.counter + 1
+      });
 
     case actionTypes.DECREMENT:
       // Easier way of immutably updating object (spread an object and then add, update whatever you want to)
-      return {
-        ...state,
+      // return {
+      //   ...state,
+      //   counter: state.counter - 1
+      // };
+
+      // Making leaner reducers using utility function (updateObject).
+      return updateObject(state, {
         counter: state.counter - 1
-      };
+      });
 
     case actionTypes.ADD:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + action.payload.value
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter + action.payload.value
+    // };
 
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - action.payload.value
-      };
+      });
+    // return {
+    //   ...state,
+    //   counter: state.counter - action.payload.value
+    // };
+
     default:
       return state;
   }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
@@ -9,13 +10,22 @@ const resultReducer = (state = initialState, action) => {
     case actionTypes.STORE_RESULT:
       // Push adds new item in original array.
       // Concat creates a copy of actual array, adds the item, and returns the new array.
-      return {
-        ...state,
+      // return {
+      //   ...state,
+      //   results: state.results.concat({
+      //     id: new Date(),
+      //     value: action.payload.result
+      //   })
+      // };
+
+      // Making leaner reducers using utility function (updateObject).
+      return updateObject(state, {
         results: state.results.concat({
           id: new Date(),
           value: action.payload.result
         })
-      };
+      });
+
     case actionTypes.DELETE_RESULT:
       // One way to immutably remove an element from an array:
       // const id = 2;
@@ -26,10 +36,16 @@ const resultReducer = (state = initialState, action) => {
       const updatedArray = state.results.filter(
         result => result.id !== action.payload.resElID
       );
-      return {
-        ...state,
+      // return {
+      //   ...state,
+      //   results: updatedArray
+      // };
+
+      // Making leaner reducers using utility function (updateObject).
+      return updateObject(state, {
         results: updatedArray
-      };
+      });
+
     default:
       return state;
   }
